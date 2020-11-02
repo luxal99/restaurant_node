4ddd50293b41eb6d4690287108c1434ba39bcbcd
Aleksa <lukicaleksa04@gmail.com>
2020-11-02 13:12:34 +0100

    Edit orm config

--- restaurant/src/index.ts
+++ restaurant/src/index.ts
@@ -2,10 +2,41 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 import {App} from "./app";
+import {Category} from "./entity/Category";
+import {Image} from "./entity/Image";
+import {Item} from "./entity/Item";
+import {Message} from "./entity/Message";
 
 
 
-createConnection().then(async connection => {
+createConnection({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "alemarc99",
+    "database": "restaurant",
+    "synchronize": true,
+    "logging": false,
+    "entities": [
+    Category,
+        Image,
+        Item,
+        Message,
+        User
+    ],
+    "migrations": [
+        "src/migration/**/*.ts"
+    ],
+    "subscribers": [
+        "src/subscriber/**/*.ts"
+    ],
+    "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+    }
+}).then(async () => {
 
 }).catch(error => console.log(error));
 
